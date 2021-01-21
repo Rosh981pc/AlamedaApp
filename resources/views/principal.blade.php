83f338a3b192077f64d3bacb6feddeab8e3dcd91
Rosh981pc <jru981@gmail.com>
2021-01-21 13:18:20 -0600

    Agregando boton de WP

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -45,6 +45,7 @@
                 <div id="concha" class=" masthead-heading text-uppercase ">A puro tubo! </div>
                 <div id="concha2" class="masthead-subheading">Tel. 7768-9199</div>
                 <a id="concha3" class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#contact">Cotizar</a>
+                <a id="concha3" class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#contact"><i class="fab fa-whatsapp"></i></a>
             </div>
         </header>
         <!-- Services-->
